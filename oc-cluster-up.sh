2470c01af4a4f2c106e2e90dd165ab7068d25a8f
Yu Shao <yu.shao.gm@gmail.com>
2016-11-23 14:43:45 -0500

    Add files via upload

--- oc-cluster-up.sh
+++ oc-cluster-up.sh
@@ -2,7 +2,4 @@ oc cluster up
 oc login -u system:admin
 oc create -f zanata-pv.yaml
 oc create -f zanata-db-pv.yaml
-oc create -f zanata-log-pv.yaml
-oc create -f zanata-tmp-pv.yaml
-oc create -f zanata-deploy-pv.yaml
-oc create -f zanata-config-pv.yaml
+oc login -u developer -p developer
