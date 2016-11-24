b757ea72b3e2f2f273aff1969a94d276af4e524d
Yu Shao <yu.shao.gm@gmail.com>
2016-11-23 21:20:40 -0500

    Update oc-cluster-up.sh

--- oc-cluster-up.sh
+++ oc-cluster-up.sh
@@ -3,3 +3,4 @@ oc login -u system:admin
 oc create -f zanata-pv.yaml
 oc create -f zanata-db-pv.yaml
 oc login -u developer -p developer
+oc process -f zanata-mariadb-localization.yaml | oc create -f -
