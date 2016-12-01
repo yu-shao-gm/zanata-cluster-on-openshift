e2f503e9f9f2c7b27c409a008453da4b6c2ec20d
root <root@localhost.localdomain>
2016-11-30 21:04:16 -0500

    update

--- oc-cluster-up.sh
+++ oc-cluster-up.sh
@@ -2,5 +2,6 @@ oc cluster up
 oc login -u system:admin
 oc create -f zanata-pv.yaml
 oc create -f zanata-db-pv.yaml
+oc adm policy add-scc-to-user anyuid -z default
 oc login -u developer -p developer
 oc process -f zanata-mariadb-localization.yaml | oc create -f -
